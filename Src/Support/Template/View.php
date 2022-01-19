248433b1734e364350c1b3590a56ba2648b52739
omarhossameldin <okandil273@gmail.com>
2022-01-20 01:53:01 +0200

    cleaing-view-render

--- Src/Support/Template/View.php
+++ Src/Support/Template/View.php
@@ -6,10 +6,11 @@ class View
 {
     private static $path = __DIR__ . '\\..\\..\\Resources\\Views\\';
 
-    public static function render($file)
+    public static function render($file, $data)
     {
+        extract($data);
         ob_start();
-        include self::$path . $file;
-        return (string) ob_get_clean();
+        include self::$path . $file . '.php';
+        echo (string) ob_get_clean();
     }
 }
