d89e8bdb57b2e6676384acaca2e84c25e34f07fb
omarhossameldin <okandil273@gmail.com>
2022-01-20 01:50:13 +0200

    cleaing-view-structre

--- Src/Resources/Views/admin/index.php
+++ Src/Resources/Views/admin/index.php
@@ -1,31 +1,8 @@
-<?php
+<link rel="stylesheet" href="<?= __DIR__ ?>/../css/jquery-ui.css">
+<link rel="stylesheet" href="<?= __DIR__ ?>/../css/chosen.min.css" />
+<link rel="stylesheet" href="<?= __DIR__ ?>/../css/style.css">
 
-use Wordpress\Resources\Views\DataSelector;
-
-$dataselect = DataSelector::selectdata();
-
-?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Create Record</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min.js" integrity="sha512-rMGGF4wg1R73ehtnxXBt5mbUfN9JUJwbk21KMlnLZDJh7BkPmeovBuddZCENJddHYYMkCh9hPFnPmS9sspki8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css" integrity="sha512-yVvxUQV0QESBt1SyZbNJMAwyKvFTLMyXSyBHDO4BG5t7k/Lw34tyqlSDlKIrIENIzCl+RVUNjmCPG+V/GMesRw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style type="text/css">
-        .wrapper {
-            width: 500px;
-            margin: 0 auto;
-        }
-    </style>
-</head>
-
-<body>
+<main>
     <div class="wrapper">
         <div class="container-fluid">
             <div class="row">
@@ -33,6 +10,7 @@ $dataselect = DataSelector::selectdata();
                     <div class="page-header">
                         <center>
                             <h1>To Do List</h1>
+
                         </center>
                     </div>
                     <center>
@@ -41,7 +19,7 @@ $dataselect = DataSelector::selectdata();
                     <center>
                         <? if (isset($_SESSION['errors']) && !empty($_SESSION['errors'])) : ?>
                             <div class="alert alert-danger">
-                                
+
                             </div>
                         <? endif; ?>
                     </center>
@@ -63,7 +41,7 @@ $dataselect = DataSelector::selectdata();
                     <form action="" method="GET">
                         <input type="text" name="page" value="to-do-list" hidden>
                         <input type="text" name="_method" value="DELETE" hidden>
-                        <center><?= $dataselect ?></center>
+                        <center><?= $test ?></center>
 
                         <div><br>
                             <center><input style="width:150px;" type="submit" class="btn btn-primary" name="submit" value="Remove Task"></center>
@@ -74,7 +52,7 @@ $dataselect = DataSelector::selectdata();
                         <div>
                             <label>Update</label>
                             <input type="hidden" name="_method" hidden>
-                            <?= $dataselect ?>
+                            <?= $test ?>
                             <input type="text" name="name" value="" class="update">
 
                         </div><br>
@@ -85,10 +63,11 @@ $dataselect = DataSelector::selectdata();
             </div>
         </div>
     </div>
-    </div>
-</body>
-<script type="text/javascript">
+</main>
+<script src="<?= __DIR__ ?>/.."></script>
+<script src="<?= __DIR__ ?>/../js/jquery-ui.min.js"></script>
+<script src="<?= __DIR__ ?>/../js/chosen.jquery.min.js"></script>
+
+<script type="text/javascript" defer>
     $(".chosen").chosen();
 </script>
-
-</html>
