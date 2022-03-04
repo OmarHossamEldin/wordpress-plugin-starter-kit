2e7cca6b6b7349f58080e7b0c87bb9d13ad39724
omarhossameldin <okandil273@gmail.com>
2022-03-05 01:23:43 +0200

    enhancement-for-plugin-init-service

--- Src/Services/InitializationService.php
+++ Src/Services/InitializationService.php
@@ -2,7 +2,8 @@
 
 namespace Wordpress\Services;
 
-use Wordpress\Database\Migrations\TasksTable;
+use Wordpress\Database\Seeders\DatabaseSeeder;
+use Wordpress\Database\Migrations\PostsTable;
 
 class InitializationService
 {
@@ -10,44 +11,31 @@ class InitializationService
     /**
      * * it's migrate database tables  and create seeders when plugin installed
      */
-
     public static function  install()
     {
         flush_rewrite_rules();
-        $tasksTable = new TasksTable;
-        $tasksTable->up();
+        $postsTable = new PostsTable();
+        $postsTable->up();
 
-        // $post = new WpPost;
-        // $post->create(['post_title'=>'post',
-        // 'post_content'=>'post']);
+        $runSeeder = new DatabaseSeeder();
+        $runSeeder->run();
+        
     }
 
     /**
      * This function deactivate the plugin
      */
     public static function deactivate()
-	{
-		flush_rewrite_rules();
-	}
+    {
+        flush_rewrite_rules();
+    }
 
-        // $runSeeder = new DatabaseSeeder;
-        // $runSeeder->run();
-    
     /**
      * This function to uninstall the plugin
      */
     public static function uninstall()
     {
-        $tasksTable = new TasksTable;
-        $tasksTable->down();
+        $postsTable = new PostsTable();
+        $postsTable->down();
     }
-
-    
-
-
 }
-
-
-// $wpdb->query( "DELETE FROM wp_posts WHERE post_type = 'Items'");
-// $wpdb->query( "DELETE FROM wp_postmeta WHERE post_id NOT IN (SELECT id From wp_posts)");
-// $wpdb->query( "DELETE FROM wp_term_relationships WHERE object_id NOT IN (SELECT id From wp_posts)");
