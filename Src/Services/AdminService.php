4d652487cddfae44b03850b348799e49bed410d0
omarhossameldin <okandil273@gmail.com>
2022-03-07 08:13:08 +0200

    finishing-admin-service

--- Src/Services/AdminService.php
+++ Src/Services/AdminService.php
@@ -3,19 +3,24 @@
 namespace Wordpress\Services;
 
 use Wordpress\Support\Facades\Router\Route;
+use Wordpress\Support\Facades\Template\Asset;
 
 class AdminService
 {
     public static function initialize()
     {
+        // register Admin assets
+        add_action('admin_enqueue_scripts', fn () => Asset::register_assets());
+
         add_action('admin_menu', function () {
             add_menu_page(
                 'posts',
                 'posts',
                 'manage_options',
-                Route::execute('PostController@index'),
                 'posts',
-                plugin_dir_url(__FILE__) . 'images/icon_wporg.png',
+                fn () =>
+                Route::execute('PostsController@index'),
+                'dashicons-welcome-write-blog',
                 110
             );
         });
