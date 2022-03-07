4048e86c59d9430b3e5bc5c4708cc5d543102f3c
omarhossameldin <okandil273@gmail.com>
2022-03-07 08:10:36 +0200

    generate-tokens-for-authentcation-usage

--- Src/Support/Facades/Faker/FakerString.php
+++ Src/Support/Facades/Faker/FakerString.php
@@ -4,7 +4,33 @@ namespace Wordpress\Support\Facades\Faker;
 
 class FakerString
 {
-    public function random_string(int $length)
+    public function first_name($gender = 'male')
     {
+    }
+
+    public function words($count = 6)
+    {
+    }
+
+    public function generate_token($length): string
+    {
+        $alphabetCaps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $alphabetSmall = 'abcdefghijklmnopqrstuvwxyz';
+        $numbers = '1234567890';
+        $key = 'WP_ARTISAN';
+        $token = '';
+
+        for ($i = 0; $i <= $length; $i++) {
+            $alphabetCapsIndex = rand(0, strlen($alphabetCaps) - 1);
+            $alphabetSmallIndex = rand(0, strlen($alphabetCaps) - 1);
+            $numbersIndex = rand(0, strlen($alphabetCaps) - 1);
+            $keyIndex = rand(0, strlen($alphabetCaps) - 1);
+            $token .=
+                $alphabetCaps[$alphabetCapsIndex] .
+                $alphabetSmall[$alphabetSmallIndex] .
+                $numbers[$numbersIndex] .
+                $key[$keyIndex];
+        }
+        return $token;
     }
 }
