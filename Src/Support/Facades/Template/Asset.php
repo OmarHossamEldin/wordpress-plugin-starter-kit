b7f3626d4beb677bfb621f1ff6cbcee4ac1ceea3
omarhossameldin <okandil273@gmail.com>
2022-03-07 08:11:01 +0200

    loading-views-assets-properly

---
+++ Src/Support/Facades/Template/Asset.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Wordpress\Support\Facades\Template;
+
+use Wordpress\Support\Facades\Filesystem\DirectoryComposer;
+
+class Asset
+{
+    /**
+     * load css files
+     *
+     * @param array $files
+     * @return void
+     */
+    public static function load_css(array $files)
+    {
+        $directoryComposer = new DirectoryComposer();
+        $cssPath = $directoryComposer->assets . "/css/";
+        foreach ($files  as $key => $file) {
+            wp_register_style($key, $cssPath . $file . '.css');
+            wp_enqueue_style($key);
+        }
+    }
+    /**
+     * load js files
+     *
+     * @param array $files
+     * @return void
+     */
+    public static function load_js(array $files)
+    {
+        $directoryComposer = new DirectoryComposer();
+        $jsPath = $directoryComposer->assets . "/js/";
+        foreach ($files  as $key => $file) {
+            wp_enqueue_script($key, $jsPath . $file . '.js');
+        }
+    }
+    /**
+     * register assets of the plugin
+     *
+     * @return void
+     */
+    public static function register_assets()
+    {
+        self::load_css([
+            'bootstrap' => 'bootstrap.min',
+            'bootstrap-rtl' => 'bootstrap.rtl.min',
+            'bootstrap-utilities' => 'bootstrap-utilities.min',
+            'bootstrap-utilities-rtl' => 'bootstrap-utilities.rtl',
+            'style' => 'style'
+        ]);
+
+        self::load_js([
+            'popper' => 'popper.min',
+            'bootstrap.min' => 'bootstrap.min',
+            'jquery' => 'jquery.min',
+            'HttpRequest' => 'HttpRequest',
+            'script' => 'script'
+        ]);
+    }
+}
